5dea3696910a2733d36596b3b454ff77576a9ced
Federico Giordani <federico@gastromia.com>
2020-10-31 16:24:50 +0100

    Included an homepage component along with optins

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </switch>
     </div>
   );
 }
