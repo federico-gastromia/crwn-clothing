7d2759dda4c440a4347e5b67e55eb4c6b2765b33
Federico Giordani <federico@gastromia.com>
2020-11-04 14:28:07 +0100

    Added email and password sign-up.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 // Convert from function to class component so that we can store the state
 class App extends React.Component {
@@ -21,8 +21,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // Check if user is signed in. UserAuth is null if not signed in
+      if (userAuth) {
+        // createUserProfileDocument creates a user profile doc in firestore if one was not already present
+        // then returns the userRef
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // Subscribe to listen to any change to the date at userRef path, but also get back a snapshot of it.
+        userRef.onSnapshot((snapShot) => {
+          // Set the state with the snapshot data.
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            }
+          });
+        });
+      }
+      
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -33,7 +51,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
